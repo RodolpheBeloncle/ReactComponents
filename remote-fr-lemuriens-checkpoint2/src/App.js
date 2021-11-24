875f6640cebc2d2c1b4ec72953e17061380d597f
RodolpheBeloncle <rbeloncle@gmail.com>
2021-11-24 09:59:07 +0100

    install context @ reducer

--- remote-fr-lemuriens-checkpoint2/src/App.js
+++ remote-fr-lemuriens-checkpoint2/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import GameList from "./component/store/GameList";
+// import Home from "./Home";
+import "tailwindcss/tailwind.css"
+import CartShopGame from "./component/cart/CartShopGame"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      
+        <>
+       
+       <Router>
+       <Header name={"Your GameShop Rodolphe"}/>
+       
+      
+        <Routes>
+        {/* <Route exact path="/" component={<Home/>}/> */}
+        <Route path="/Games" element={<GameList/>}/>
+        <Route path="/Cart" element={<CartShopGame/>}/>
+         
+
+        </Routes>
+
+       </Router>
+      </>
+    
   );
 }
 
